049fb2ec055d3008e5d3c1a063a6a7e0c3a829a5
arcanjobento <arcanjomalengue@hotmail.com>
2025-11-20 09:46:49 +0100

    Adicionar arquivos de projeto.

---
+++ TextesMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TextesMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TextesMaui/ViewModel/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextesMaui.ViewModel
+{
+    internal class MainViewModel
+    {
+    }
+}
